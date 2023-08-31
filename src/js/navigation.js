efb24ec20ef918435446eb23b4716b2d25ca1894
Arturo Mendoza <91037768+dizsel74@users.noreply.github.com>
2023-08-31 16:38:14 -0600

    boton de navegacion

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
 console.log('JavaScript file loaded');
 
 const primaryNav = document.querySelector('.nav__list');
@@ -21,4 +22,33 @@ navToggle.addEventListener('click', () => {
     iconClose.style.display='none';
     iconHamburger.style.display='block';
   }
+});
+
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  console.log('click');
+}
+
+// Attach the scroll-to-top functionality to the button
+mybutton.addEventListener('click', topFunction);
 });
